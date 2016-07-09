d71402882062ff7729c066bac69096d9fe0b5f19
ErChivo <chivo5927@gmail.com>
2016-07-09 16:15:27 -0430

    Prueba volver menu

---
+++ Game/Assets/Flecha/InicioScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InicioScript : MonoBehaviour {
+	
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadAddOnClick(int Menu)
+	{
+		Application.LoadLevelAdditive("Menu");
+	}
+}
