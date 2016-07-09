f92ab07e77ba9cddb8700b687b4f86788fd05c6b
Pchyng <pchyng@users.noreply.github.com>
2016-07-09 18:13:18 +0200

    Sonido persistente arreglado para lvl tesis via xml

--- Game/Assets/SCRIPT/Controllers/MutearSonido.cs
+++ Game/Assets/SCRIPT/Controllers/MutearSonido.cs
@@ -30,21 +30,10 @@ public class MutearSonido : MonoBehaviour {
 			var lStatus = lConfigCollection.getConfigs (0).Status;
 			mute = lStatus;
 		} 
-
-
-
-
-
-
-
-//		var lPath = Application.streamingAssetsPath + "!/assets/Scores/config.xml";
-
-		//var lConfigCollection = ConfigManager.Load(lPath);
-
-		//cONGIFURANDO SONIDO, VALORES PERMITIDOS EN config.xml true = un-mute & false = mute
-		//var lStatus = lConfigCollection.getConfigs (0).Status;
-		//mute = lStatus;
-
+		AudioSource source2 = GameObject.Find("Camara").GetComponent<AudioSource>();
+		AudioSource source3 = GameObject.Find("Jugador").GetComponent<AudioSource>();
+		source2.mute=mute;
+		source3.mute=mute;
 	}
 	
 	public void handleMute(){
