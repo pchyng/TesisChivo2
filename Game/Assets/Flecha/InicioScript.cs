8d1c21a1e4fd86fce922e36dc2b2a090566778f1
Pchyng <pchyng@users.noreply.github.com>
2016-07-09 22:58:48 +0200

    Arreglado lo del mnu

--- Game/Assets/Flecha/InicioScript.cs
+++ Game/Assets/Flecha/InicioScript.cs
@@ -2,21 +2,11 @@
 using System.Collections;
 
 public class InicioScript : MonoBehaviour {
-	
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	public void LoadAddOnClick(int Menu)
 	{
-		Application.LoadLevelAdditive("Menu");
+		Application.LoadLevel(0);
+
+	//	Application.LoadLevelAdditive("Menu");
 	}
 }
