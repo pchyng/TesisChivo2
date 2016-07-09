5935599cffce2a5fb086215dd1b2504b5f099c84
Pchyng <pchyng@users.noreply.github.com>
2016-07-09 19:49:57 +0200

    Intento score 1 crea el archivo no recupera

--- Game/Assets/SCRIPT/Galleta y Puntaje/Puntaje.cs
+++ Game/Assets/SCRIPT/Galleta y Puntaje/Puntaje.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System;
+using System.Xml;
 
 public class Puntaje : MonoBehaviour {
 	public GUIText guiText;
@@ -11,7 +13,9 @@ public class Puntaje : MonoBehaviour {
 	public float dist = 0f;
 	static bool saved = false;
 	public Transform other;
-	
+
+	string lPath;
+
 	static Puntaje instance;
 
 	static public void AddPoint(){
@@ -22,6 +26,7 @@ public class Puntaje : MonoBehaviour {
 			highScore = score;
 		}
 	}
+
 	PrincesavueScript princesa;
 	
 	void Start(){
@@ -55,29 +60,99 @@ public class Puntaje : MonoBehaviour {
 		} else {
 			if(!saved){
 				saved=true;
-				var lPath = Application.streamingAssetsPath + "/Scores/config.xml";
+				lPath = Application.streamingAssetsPath + "/Scores/config.xml";
 				lPath= ConfigManager.getPath("scores.xml");
-
-				//var scoreCollection = ScoreList.Load(Path.Combine(Path.Combine(Application.dataPath,"Scores"),"scores.xml"));
-				var scoreCollection = ScoreList.Load(lPath);
-				//				Debug.LogWarning(serializer.Deserialize(stream));
-				var lScore = new Score ();
-				int auxPints = (int)(dist);
-				
-				for (int i = 0; i < 5; i++) {
-					lScore = scoreCollection.getScores(i);
-					Debug.LogWarning(lScore.Name);
-					Debug.LogWarning(lScore.Points);
-					if(lScore.Points<=auxPints){
-						int aux2 = lScore.Points; 
-						lScore.Points = auxPints;
-						auxPints = aux2;
-					}
-				}
-				scoreCollection.Save(lPath,0);
-				guiText.text ="";
 			}
+			try {
+				var lConfigCollection = ConfigManager.Load(lPath);
+				var lStatus = lConfigCollection.getConfigs (0);
+				}
+			catch (Exception e) {
+				createXML (lPath);
+				var lConfigCollection = ConfigManager.Load(lPath);
+				var lStatus = lConfigCollection.getConfigs (0);
+			} 
+			//var scoreCollection = ScoreList.Load(Path.Combine(Path.Combine(Application.dataPath,"Scores"),"scores.xml"));
+			var scoreCollection = ScoreList.Load(lPath);
+			//				Debug.LogWarning(serializer.Deserialize(stream));
+			var lScore = new Score ();
+			int auxPints = (int)(dist);
 			
+			for (int i = 0; i < 5; i++) {
+				lScore = scoreCollection.getScores(i);
+				Debug.LogWarning(lScore.Name);
+				Debug.LogWarning(lScore.Points);
+				if(lScore.Points<=auxPints){
+					int aux2 = lScore.Points; 
+					lScore.Points = auxPints;
+					auxPints = aux2;
+				}
+			}
+			scoreCollection.Save(lPath,0);
+			guiText.text ="";
+			}
+
 		}
+
+	static void createXML(string path) {
+		XmlDocument doc = new XmlDocument( );
+		
+		//(1) the xml declaration is recommended, but not mandatory
+		XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration( "1.0", "Windows-1252", null );
+		XmlElement root = doc.DocumentElement;
+		doc.InsertBefore( xmlDeclaration, root );
+		
+		//(2) string.Empty makes cleaner code
+		XmlElement element0 = doc.CreateElement( string.Empty, "ScoreListCollection", string.Empty );
+		element0.SetAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
+		element0.SetAttribute("xmlns:xsd","http://www.w3.org/2001/XMLSchema");
+		doc.AppendChild( element0 );
+		
+		XmlElement element1 = doc.CreateElement( string.Empty, "Listado", string.Empty );
+		element0.AppendChild( element1 );
+		
+		XmlElement elementPN1 = doc.CreateElement( string.Empty, "Items",  string.Empty );
+		elementPN1.SetAttribute("name","Primero");
+		element1.AppendChild( elementPN1 );
+		XmlElement elementPP1 = doc.CreateElement( string.Empty, "Points", string.Empty );
+		XmlText text1 = doc.CreateTextNode( "5" );
+		elementPP1.AppendChild( text1 );
+		elementPN1.AppendChild( elementPP1 );
+
+		XmlElement elementPN2 = doc.CreateElement( string.Empty, "Items",  string.Empty );
+		elementPN2.SetAttribute("name","Segundo");
+		element1.AppendChild( elementPN2 );
+		XmlElement elementPP2 = doc.CreateElement( string.Empty, "Points", string.Empty );
+		XmlText text2 = doc.CreateTextNode( "4" );
+		elementPP2.AppendChild( text2 );
+		elementPN2.AppendChild( elementPP2 );
+
+		XmlElement elementPN3 = doc.CreateElement( string.Empty, "Items",  string.Empty );
+		elementPN3.SetAttribute("name","Tercero");
+		element1.AppendChild( elementPN3 );
+		XmlElement elementPP3 = doc.CreateElement( string.Empty, "Points", string.Empty );
+		XmlText text3 = doc.CreateTextNode( "3" );
+		elementPP3.AppendChild( text3 );
+		elementPN3.AppendChild( elementPP3 );
+
+		XmlElement elementPN4 = doc.CreateElement( string.Empty, "Items",  string.Empty );
+		elementPN4.SetAttribute("name","Cuarto");
+		element1.AppendChild( elementPN4 );
+		XmlElement elementPP4 = doc.CreateElement( string.Empty, "Points", string.Empty );
+		XmlText text4 = doc.CreateTextNode( "2" );
+		elementPP4.AppendChild( text4 );
+		elementPN4.AppendChild( elementPP4 );
+
+		XmlElement elementPN5 = doc.CreateElement( string.Empty, "Items",  string.Empty );
+		elementPN5.SetAttribute("name","Quinto");
+		element1.AppendChild( elementPN5 );
+		XmlElement elementPP5 = doc.CreateElement( string.Empty, "Points", string.Empty );
+		XmlText text5 = doc.CreateTextNode( "1" );
+		elementPP5.AppendChild( text5 );
+		elementPN5.AppendChild( elementPP5 );
+
+		doc.Save( path );
 	}
-}
+	}
+	
+
