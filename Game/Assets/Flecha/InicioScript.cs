3934340111c987e35a5b27878c3282a9b9ed183b
ErChivo <chivo5927@gmail.com>
2016-07-10 16:05:29 -0430

    Resuelto inicio y assets

--- Game/Assets/Flecha/InicioScript.cs
+++ Game/Assets/Flecha/InicioScript.cs
@@ -2,11 +2,22 @@
 using System.Collections;
 
 public class InicioScript : MonoBehaviour {
+	
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
 	public void LoadAddOnClick(int Menu)
 	{
 		Application.LoadLevel(0);
-
-	//	Application.LoadLevelAdditive("Menu");
+		//Application.LoadLevelAdditive("Menu");
 	}
 }
