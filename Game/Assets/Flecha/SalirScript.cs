78795e51068fe0a58e113cefed37b1074ca9c1a6
ErChivo <chivo5927@gmail.com>
2016-07-20 20:50:22 -0430

    boton de salida

---
+++ Game/Assets/Flecha/SalirScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SalirScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Application.Quit ();
+	}
+
+	public void onClick(){
+		Application.Quit ();
+	}
+
+
+}
